2a44dea44a01b74a264e4d053226a8f131fb036c
natalina4kai-BSBSJ <natalina4kai@gmail.com>
2025-11-03 01:05:11 +0100

    seve Lesson4.6.py

---
+++ lesson4.6.py
@@ -0,0 +1,23 @@
+num = 10
+name = "Tom"
+
+result = num > 5 and name == "Tom"
+print(result)
+
+num = 10
+name = "Tom"
+
+result = num < 5 or name == "Tom"
+print(result)
+
+num = 10
+name = "Tom"
+
+result = num < 5 or name == "Jon"
+print(result)
+
+num = 10
+name = "Tom"
+
+result = num < 5 and name == "Tom"
+print(result)
