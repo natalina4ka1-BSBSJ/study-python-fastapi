626640cebf6d053d82543f41ea8da0cc85e92e05
natalina4kai-BSBSJ <natalina4kai@gmail.com>
2026-02-08 23:20:28 +0100

    lesson4.2.py

---
+++ lesson4.2.py
@@ -0,0 +1,27 @@
+num_1 = 100
+num_2 = 10
+num_3 = num_1 + num_2
+# Переменные и математические действия.
+# Variables Math Operations.
+# Variablen und Mathematische Operationen.
+print(num_3)
+
+num_1 = 100
+num_2 = 10
+
+print(num_1 + num_2)
+
+num_1 = 100
+num_2 = 10
+
+print(num_1 - num_2)
+
+num_1 = 100
+num_2 = 10
+
+print(num_1 * num_2)
+
+num_1 = 100
+num_2 = 10
+
+print(num_1 / num_2)
