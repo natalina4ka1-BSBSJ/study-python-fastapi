0ec607cf36bc76bfe046e412cdb74bd5d92a78c3
natalina4kai-BSBSJ <natalina4kai@gmail.com>
2026-02-11 00:04:10 +0100

    WIP: Lesson4.4.py

---
+++ lesson4.4.py
@@ -0,0 +1,56 @@
+num_1 = 7
+num_2 = 2
+
+
+
+num_3 = num_1 / num_2
+
+# Division
+# Ділення
+
+num_4 = num_1 // num_2
+
+# Floor Division
+# Цілісній поділ
+
+print(num_3, num_4)
+
+
+
+num_1 = 5
+num_2 = 2
+
+# Exponentiation
+# Potenzierung
+# Введення у ступінь
+
+num_3 = num_1 ** num_2
+print(num_3)
+
+# Example
+# Beispiel 
+# Приклад
+
+knowledge = 10
+time = 3
+result_1 = knowledge / time
+
+# This is detailed analysis: how much theory is learned per hour.
+
+# das ist eine detaillierte Analyse: Wie viel Theorie wird pro Stunde gelernt.
+
+# Це детальний аналіз:  скільки теорії засвоєно за годину. 
+
+result_2 = knowledge // time
+
+
+result_3 = knowledge ** time
+
+# we take volume of knowledge (10) and time (3)
+
+# Wie nehmen das Wissenvolumen (10) und die Zeit (3)
+
+# Ми беремо обєм знань (10) та час (3)
+
+
+
