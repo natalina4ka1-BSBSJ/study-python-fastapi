900ee7e05cfbf2ad2f7232f2cd5b895e577c92a2
natalina4kai-BSBSJ <natalina4kai@gmail.com>
2026-02-12 00:27:44 +0100

    completed Lesson4.4.py

--- lesson4.4.py
+++ lesson4.4.py
@@ -1,31 +1,25 @@
 num_1 = 7
 num_2 = 2
 
-
-
-num_3 = num_1 / num_2
-
 # Division
 # Ділення
-
-num_4 = num_1 // num_2
+num_3 = num_1 / num_2
+print(num_3)
 
 # Floor Division
 # Цілісній поділ
-
-print(num_3, num_4)
-
-
-
-num_1 = 5
-num_2 = 2
+num_4 = num_1 // num_2
+print(num_4)
 
 # Exponentiation
 # Potenzierung
 # Введення у ступінь
 
+num_1 = 5
+num_2 = 2
 num_3 = num_1 ** num_2
 print(num_3)
+
 
 # Example
 # Beispiel 
@@ -33,18 +27,6 @@ print(num_3)
 
 knowledge = 10
 time = 3
-result_1 = knowledge / time
-
-# This is detailed analysis: how much theory is learned per hour.
-
-# das ist eine detaillierte Analyse: Wie viel Theorie wird pro Stunde gelernt.
-
-# Це детальний аналіз:  скільки теорії засвоєно за годину. 
-
-result_2 = knowledge // time
-
-
-result_3 = knowledge ** time
 
 # we take volume of knowledge (10) and time (3)
 
@@ -52,5 +34,39 @@ result_3 = knowledge ** time
 
 # Ми беремо обєм знань (10) та час (3)
 
+#-------------------------------------------
+
+# Division (float (3,3333))
+
+# This is detailed analysis: how much theory is learned per hour.
+
+# das ist eine detaillierte Analyse: Wie viel Theorie wird pro Stunde gelernt.
+
+# Це детальний аналіз:  скільки теорії засвоєно за годину. 
+
+result_1 = knowledge / time
+
+# Floor Division (3)
+
+# This is the "solid residue": 
+# only knowledge that became a practical skill.
+
+# Dies ist der "feste Rükstand": nur Wissen, 
+# das zu einer pracktischen fertigkeit wurde
+
+# Це лише "твердий залишок" ті знання, 
+# що перетворилися на практичні навички.
+
+result_2 = knowledge // time
+
+# Exponentiation
+
+# This is synergy: when honesty and hard work square your Kapital.
+
+# Das ist Synergie: wenn Ehrlichkeit und harte Arbeit Ihr Kapital quadrieren.
+
+# Це синергія: коли чесність та робота,  підносить ваш капітіл у квадрат.
+
+result_3 = knowledge ** time
 
 
